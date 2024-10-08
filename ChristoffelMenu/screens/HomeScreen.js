c708808764453c0dae326cf86469882d5f7b5985
ST10450884@ <ST10450884@vcconnect.edu.za>
2024-10-08 21:04:37 +0200

    Feature: Allow removal of menu items from Home Screen

--- ChristoffelMenu/screens/HomeScreen.js
+++ ChristoffelMenu/screens/HomeScreen.js
@@ -1,19 +1,18 @@
-import React, { useState } from 'react';
-import { View, Text, FlatList, Button } from 'react-native';
-
-const HomeScreen = ({ menuItems }) => {
-  return (
-    <View>
-      <Text>Total Menu Items: {menuItems.length}</Text>
-      <FlatList
-        data={menuItems}
-        keyExtractor={(item) => item.name}
-        renderItem={({ item }) => (
-          <Text>{item.name} - {item.course}</Text>
-        )}
-      />
-    </View>
-  );
-};
-
-export default HomeScreen;
+const HomeScreen = ({ menuItems, removeMenuItem }) => {
+    return (
+      <View>
+        <Text>Total Menu Items: {menuItems.length}</Text>
+        <FlatList
+          data={menuItems}
+          keyExtractor={(item) => item.name}
+          renderItem={({ item }) => (
+            <View>
+              <Text>{item.name} - {item.course}</Text>
+              <Button title="Remove" onPress={() => removeMenuItem(item.name)} />
+            </View>
+          )}
+        />
+      </View>
+    );
+  };
+  
